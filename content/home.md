f82df092a00ead341eedf6b84f46532f143a9512
digitone <digitone@naver.com>
2022-01-03 15:52:23 +0900

    111

---
+++ content/home.md
@@ -0,0 +1,15 @@
+---
+title: Awesome kitties
+date: 2019-03-17T19:31:20.591Z
+cats:
+  - name: Maru (まる)
+    description: 'Maru is a Scottish Fold from Japan, and he loves boxes.'
+  - name: 'kwon'
+    description: Lil Bub is an American celebrity cat known for her unique appearance.
+  - name: Grumpy cat (Tardar Sauce)
+    description: 'Grumpy cat is an American celebrity cat known for her grumpy appearance.'
+    
+---
+Welcome to my awesome page about cats of the internet.
+
+This page is built with NextJS, and content is managed in Netlify CMS
---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import { attributes, react as HomeContent } from '../content/home.md';
+
+export default function Home() {
+  let { title, cats, date } = attributes;
+
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+              <p>{date}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  )
+}
